5a9c637439c250b184e92a835d63df8156be13a6
Gabriel Thomé <gabrielallan.thome@gmail.com>
2023-03-19 22:05:22 -0300

    decompiling listing_0039

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,34 @@
 use std::env;
-use std::fs::read;
+use std::fs::{read, write};
 
 const BYTE_REGISTERS: [&str; 8] = ["al", "cl", "dl", "bl", "ah", "ch", "dh", "bh"];
 const WORD_REGISTERS: [&str; 8] = ["ax", "cx", "dx", "bx", "sp", "bp", "si", "di"];
+const REGISTER_ENCODINGS: [[&str; 8]; 2] = [BYTE_REGISTERS, WORD_REGISTERS];
+
+const RM_ADDRESS_CALCULATION_ENCODINGS: [&str; 8] = [
+    "[bx + si]",
+    "[bx + di]",
+    "[bp + si]",
+    "[bp + di]",
+    "[si]",
+    "[di]",
+    "", // to own the libs
+    "[bx]",
+];
+
+fn rm_address_calculation_displaced(rm_bits: &u8, displacement: &u16) -> String {
+    match rm_bits {
+        0x0 => format!("[bx + si + {displacement}]"),
+        0x1 => format!("[bx + di + {displacement}]"),
+        0x2 => format!("[bp + si + {displacement}]"),
+        0x3 => format!("[bp + di + {displacement}]"),
+        0x4 => format!("[si + {displacement}]"),
+        0x5 => format!("[di + {displacement}]"),
+        0x6 => format!("[bp + {displacement}]"),
+        0x7 => format!("[bx + {displacement}]"),
+        _ => "".to_owned(),
+    }
+}
 
 enum Mov {
     RegisterOrMemoryToOrFromRegister,
@@ -51,32 +77,35 @@ fn parse_register_or_memory_to_or_from_register(bytes: &Vec<u8>, cursor: &mut us
     let second_byte = bytes[*cursor + 1];
     *cursor += 2;
 
-    let d_bit = first_byte & 0x2;
+    let d_bit = (first_byte >> 1) & 0x1;
     let w_bit = first_byte & 0x1;
 
     let r#mod = second_byte >> 6;
-    let reg = (second_byte & 0b00_111_000) >> 3;
-    let rm = second_byte & 0b00_000_111;
+    let register_bits = (second_byte >> 3) & 0x7;
+    let rm_bits = second_byte & 0x7;
 
-    if r#mod != 0x3 {
-        panic!("Unsupported operation. We only deal with register-to-register instructions for now. Come back later!");
-    }
+    let register = REGISTER_ENCODINGS[w_bit as usize][register_bits as usize];
 
-    let destination = if d_bit == 1 { reg } else { rm };
-    let source = if d_bit == 1 { rm } else { reg };
-
-    let source_register = if w_bit == 1 {
-        WORD_REGISTERS[source as usize]
-    } else {
-        BYTE_REGISTERS[source as usize]
+    let rm = match r#mod {
+        0x0 => RM_ADDRESS_CALCULATION_ENCODINGS[rm_bits as usize].to_owned(),
+        0x1 => {
+            let displacement = bytes[*cursor];
+            *cursor += 1;
+            rm_address_calculation_displaced(&rm_bits, &(displacement as u16))
+        }
+        0x2 => {
+            let displacement = u16::from_ne_bytes([bytes[*cursor], bytes[*cursor + 1]]);
+            *cursor += 2;
+            rm_address_calculation_displaced(&rm_bits, &displacement)
+        }
+        0x3 => REGISTER_ENCODINGS[w_bit as usize][rm_bits as usize].to_owned(),
+        _ => "".to_owned(),
     };
-    let destination_register = if w_bit == 1 {
-        WORD_REGISTERS[destination as usize]
-    } else {
-        BYTE_REGISTERS[destination as usize]
-    };
 
-    String::from(format!("mov {destination_register}, {source_register}"))
+    let destination = if d_bit == 1 { register } else { &rm };
+    let source = if d_bit == 1 { &rm } else { register };
+
+    String::from(format!("mov {destination}, {source}"))
 }
 
 fn parse_immediate_to_register(bytes: &Vec<u8>, cursor: &mut usize) -> String {
@@ -109,12 +138,11 @@ fn main() {
         panic!("No filename provided");
     }
 
-    let file = read(&args[1]).expect("could not read file");
+    let file = read(&args[1]).expect("could not read input file");
 
     let mut cursor = 0;
     let mut asm = String::from("bits 16\n\n");
     while cursor < file.len() {
-        println!("asm: {asm}");
         let first_byte = file[cursor];
 
         let op = as_mov_enum(first_byte)
@@ -139,5 +167,14 @@ fn main() {
         }
     }
 
-    println!("{asm}");
+    if args.contains(&String::from("--stdio")) {
+        println!("{asm}");
+        return;
+    }
+
+    // maybe in the future I'll write a proper args parser 
+    // and then add a -o, --output argument and only 
+    // generate an output file if it's set and use its
+    // value as the output file name
+    write("output", &asm).expect("error trying to write to file");
 }
